4a35b8caaf366152e2afb440b1442838a4dddbb1
Sokhavuth TIN <vuthdevelop@gmail.com>
2021-04-16 21:08:43 +0700

    Make it better

--- mytv/settings.py
+++ mytv/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-$%2+2^=l!#jtc#u#an)4g%nhn=0r2zzcg)7mz7jl!27vdq&0!p
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["khmerweb-polls.herokuapp.com"]
 
 
 # Application definition
