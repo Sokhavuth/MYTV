77f45b65e4c119ce398cea0e9dc45bb53f403fb8
Sokhavuth TIN <vuthdevelop@gmail.com>
2021-04-16 19:10:16 +0700

    Make it better

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,10 @@
 from django.urls import include, path
-#from django.contrib import admin
+from django.contrib import admin
 
 from . import views
 
 urlpatterns = [
     path('', views.index),
     path('polls/', include('polls.urls')),
-    #path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
 ]
