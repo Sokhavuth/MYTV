e093c4489952051f524bc52095954ad013513c4e
Sokhavuth TIN <vuthdevelop@gmail.com>
2021-04-16 18:17:28 +0700

    Make it better

--- index.py
+++ index.py
@@ -4,5 +4,3 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pollsapi.settings")
 app = get_wsgi_application()
 
-os.system("python manage.py migrate")
-os.system("python manage.py runserver")
